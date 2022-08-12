c20b5ea2d3bba915dd1d23578e21b813f01c457f
Vikash Roy <87648415+vikash03a@users.noreply.github.com>
2022-08-12 11:44:02 +0530

    Update index.js
    Done

--- src/index.js
+++ src/index.js
@@ -12,35 +12,12 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    {/* <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="/posts" element={<Posts />} />
-          <Route path="/comments" element={<Comments />} />
-        </Route>
-      </Routes>
-    </BrowserRouter> */}
     <BrowserRouter>
-      <div className="App">
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">App</Link>
-            </li>
-            <li>
-              <Link to="/Posts">Posts</Link>
-            </li>
-            <li>
-              <Link to="/Comments">Comments</Link>
-            </li>
-          </ul>
-        </nav> */}
         <Routes>
           <Route path="/" element={<App />}></Route>
           <Route path="/posts" element={<Posts />}></Route>
           <Route path="/comments" element={<Comments />}></Route>
         </Routes>
-      </div>
     </BrowserRouter>
   </StrictMode>
 );
